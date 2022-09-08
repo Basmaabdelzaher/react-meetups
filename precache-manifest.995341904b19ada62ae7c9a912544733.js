72c1266f39c497c0e3bd62c46d98e4924bfcec94
Basma Abd Elzaher <basmaabdelzaher@gmail.com>
2022-09-08 10:04:06 +0200

    Updates

---
+++ precache-manifest.995341904b19ada62ae7c9a912544733.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4339740bf12b93f7524c5882c4c90f5",
+    "url": "/react-meetups/index.html"
+  },
+  {
+    "revision": "50fb52d3b7ebc28bac80",
+    "url": "/react-meetups/static/css/main.69bd7a18.chunk.css"
+  },
+  {
+    "revision": "e304c4545cf3c2565ac9",
+    "url": "/react-meetups/static/js/2.ec23232f.chunk.js"
+  },
+  {
+    "revision": "50fb52d3b7ebc28bac80",
+    "url": "/react-meetups/static/js/main.989a1181.chunk.js"
+  },
+  {
+    "revision": "21d60d1df2af41dc6e88",
+    "url": "/react-meetups/static/js/runtime~main.f48a4009.js"
+  }
+]);
